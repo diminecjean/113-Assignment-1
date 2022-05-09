499cb5bf7dd1b537f41961e0cb18efbfe0067bff
diminecjean <93825624+diminecjean@users.noreply.github.com>
2022-05-09 23:26:32 +0800

    Update Form.h

--- Form.h
+++ Form.h
@@ -7,13 +7,13 @@
 
 using namespace std;
 
-const int size = 1;
+//const int size = 1;
 
 class Form
 {
 	// scope of variables and function operations still to be decided
 	private:
-		Student stud[size]; // Student object (Composition)(array of objects)
+		Student stud; // Student object (Composition)(array of objects)
 		bool Vacc_Status, CovidTest;
 		string Vacc_Type, campus;
 		int dateIn, dateOut;
@@ -23,7 +23,7 @@ class Form
 		Form();
 		
 		// overloaded constructor
-		Form (bool, bool, string, string, int, int);
+		//Form (bool, bool, string, string, int, int);
 		
 		// Mutators
 		void setVacc_Status(bool);
@@ -32,7 +32,8 @@ class Form
 		void setCampus(string);
 		void setDateIn(int);
 		void setDateOut(int);
-		void setStudentInfo(string, int, int, string, string, bool, long int, string);
+		void setStudentInfo(string, string, int, int, string, string, bool, long long int, string);
+		void setForm (bool, bool, string, string, int, int);
 		
 		// Accessors
 		bool getVacc_Status();
@@ -59,15 +60,15 @@ Form::Form()
 }
 
 // Overloaded Constructor -------------------------------------------------
-Form::Form(bool Vacc_Status, bool CovidTest, string Vacc_Type, string campus, int dateIn, int dateOut)
-{
-	this -> Vacc_Status = Vacc_Status;
-	this -> CovidTest = CovidTest;
-	this -> Vacc_Type= Vacc_Type;
-	this -> campus = campus;
-	this -> dateIn = dateIn;
-	this -> dateOut = dateOut;
-}
+//Form::Form(bool Vacc_Status, bool CovidTest, string Vacc_Type, string campus, int dateIn, int dateOut)
+//{
+//	this -> Vacc_Status = Vacc_Status;
+//	this -> CovidTest = CovidTest;
+//	this -> Vacc_Type= Vacc_Type;
+//	this -> campus = campus;
+//	this -> dateIn = dateIn;
+//	this -> dateOut = dateOut;
+//}
 
 // Mutators  ------------------------------------------------------------
 void Form::setVacc_Status(bool Vacc_Status)
@@ -100,19 +101,24 @@ void Form::setDateOut(int dateOut)
 	this -> dateOut = dateOut;
 }
 
-void Form::setStudentInfo(string name, int ID, int year, string school, string programme, bool status, long int phone, string email)
+void Form::setForm(bool Vacc_Status, bool CovidTest, string Vacc_Type, string campus, int dateIn, int dateOut)
 {
-	for (int i =0; i<size; i++)
-	{
-		stud[i].setName(name);
-		stud[i].setID(ID);
-		stud[i].setYear(year);
-		stud[i].setSchool(school);
-		stud[i].setProgramme(programme);
-		stud[i].setStatus(status);
-		stud[i].setPhone(phone);
-		stud[i].setEmail(email);
-	}
+	this -> Vacc_Status = Vacc_Status;
+	this -> CovidTest = CovidTest;
+	this -> Vacc_Type= Vacc_Type;
+	this -> campus = campus;
+	this -> dateIn = dateIn;
+	this -> dateOut = dateOut;
+}
+
+void Form::setStudentInfo(string name, string NRIC, int ID, int year, string school, string programme, bool status, long long int phone, string email)
+{
+//	for (int i =0; i<size; i++)
+//	{
+//		stud[i].setInfo(name, NRIC, ID, year, school, programme, status, phone, email);
+//	}
+
+	stud.setInfo(name, NRIC, ID, year, school, programme, status, phone, email);
 }
 
 // Accessors -----------------------------------------------------------------
@@ -148,11 +154,31 @@ int Form::getDateOut()
 		
 void Form::print()
 {
-	for (int i =0; i<size; i++)
-	{
-		if (stud[i].getStatus()==false)
+//	for (int i =0; i<size; i++)
+//	{
+//		if (stud[i].getStatus()==true)
+//		{
+//			stud[i].print();
+//			
+//			cout << "Vaccine Status: " << getVacc_Status() <<endl;
+//			cout << "Covid Test Results: " << getCovidTest() <<endl;
+//			cout << "Vaccine Type (first booster): " << getVacc_Type() <<endl;
+//			cout << "USM Campus: " << getCampus() <<endl;
+//			cout << "Date In: " << getDateIn() <<endl;
+//			cout << "Date Out: " << getDateOut() <<endl;
+//			
+//			system("pause");
+//		}
+//		else
+//		{
+//			cout << stud[i].getName() <<" is not an active student in USM. Stay not permitted." <<endl;
+//			
+//			system("pause");
+//		}
+//	}
+		if (stud.getStatus()==true)
 		{
-			stud[i].print();
+			stud.print();
 			
 			cout << "Vaccine Status: " << getVacc_Status() <<endl;
 			cout << "Covid Test Results: " << getCovidTest() <<endl;
@@ -162,14 +188,15 @@ void Form::print()
 			cout << "Date Out: " << getDateOut() <<endl;
 			
 			system("pause");
+			system("CLS");
 		}
 		else
 		{
-			cout << stud[i].getName() <<" tested positive for Covid-19. Stay is not allowed." <<endl;
+			cout << stud.getName() <<" is not an active student in USM. Stay not permitted." <<endl;
 			
 			system("pause");
+			system("CLS");
 		}
-	}
 
 }
 
